619a069568cd1d19625f0da89bfc72e319222a79
Sonja Feitsch <feitsch@e2n.de>
2024-01-04 10:39:36 +0100

    restructure code

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 /**
  * Base
  */
-let textValue = "Hello Three.js";
+let guiValues = { text: "Hello Three.js" };
 let font;
 let camera, scene, controls, renderer;
 
-let group, textGeometry, textMesh, material;
+let group, textGeometry, textMesh, material, matcapTexture;
 
 // Debug
 const gui = new GUI();
@@ -25,9 +25,15 @@ const sizes = {
   height: window.innerHeight,
 };
 
+init();
+animate();
+
 function init() {
   // Canvas
   const canvas = document.querySelector("canvas.webgl");
+
+  // Scene
+  scene = new THREE.Scene();
 
   // Camera
   camera = new THREE.PerspectiveCamera(
@@ -41,73 +47,45 @@ function init() {
   camera.position.z = 2;
   scene.add(camera);
 
-  // Scene
-  const scene = new THREE.Scene();
+  // Controls
+  controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+
+  // Render
+  renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  //Group
+  gui
+    .add(guiValues, "text")
+    .name("Text")
+    .onChange((newText) => {
+      guiValues.text = newText;
+      refreshText();
+    });
+
+  loadTextures();
+  loadFont();
+
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 40);
+  material = new THREE.MeshMatcapMaterial();
+  material.matcap = matcapTexture;
+
+  for (let i = 0; i < 300; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+    scene.add(donut);
+  }
 }
-
-// Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
-scene = new THREE.Scene();
-
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("/textures/matcaps/custom.png");
-matcapTexture.colorSpace = THREE.SRGBColorSpace;
-
-/**
- * Fonts
- */
-loadFont();
-
-const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 40);
-material = new THREE.MeshMatcapMaterial();
-material.matcap = matcapTexture;
-
-for (let i = 0; i < 300; i++) {
-  const donut = new THREE.Mesh(donutGeometry, material);
-  donut.position.x = (Math.random() - 0.5) * 10;
-  donut.position.y = (Math.random() - 0.5) * 10;
-  donut.position.z = (Math.random() - 0.5) * 10;
-
-  donut.rotation.x = Math.random() * Math.PI;
-  donut.rotation.y = Math.random() * Math.PI;
-
-  const scale = Math.random();
-  donut.scale.set(scale, scale, scale);
-  scene.add(donut);
-}
-
-/**
- * Camera
- */
-// Base camera
-camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.x = 1;
-camera.position.y = 1;
-camera.position.z = 2;
-scene.add(camera);
-
-// Controls
-controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-
-/**
- * Renderer
- */
-renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Animate
- */
-animate();
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -122,6 +100,12 @@ function render() {
   renderer.render(scene, camera);
 }
 
+function loadTextures() {
+  const textureLoader = new THREE.TextureLoader();
+  matcapTexture = textureLoader.load("/textures/matcaps/custom.png");
+  matcapTexture.colorSpace = THREE.SRGBColorSpace;
+}
+
 function loadFont() {
   const loader = new FontLoader();
   loader.load("/fonts/optimer_regular.typeface.json", function (response) {
@@ -131,15 +115,15 @@ function loadFont() {
 }
 
 function refreshText() {
-  // group.remove(textMesh);
+  scene.remove(textMesh);
 
-  if (!textValue) return;
+  if (!guiValues.text) return;
 
   createText();
 }
 
 function createText() {
-  textGeometry = new TextGeometry(textValue, {
+  textGeometry = new TextGeometry(guiValues.text, {
     font,
     size: 0.5,
     height: 0.2,
